95b8254a7a5d94e6d93d7137d1cf7079204d56b6
Victor <victorgaspalmeida@hotmail.com>
2023-07-04 20:19:19 -0300

    feat: create UserSchema

---
+++ src/main/java/com/Reviewz/infrastructure/config/db/schema/UserSchema.java
@@ -0,0 +1,54 @@
+package com.Reviewz.infrastructure.config.db.schema;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserSchema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+
+	public UserSchema(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public UserSchema() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	
+}
