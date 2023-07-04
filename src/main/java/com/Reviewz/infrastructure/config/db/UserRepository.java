a69dd332d386d4fd9a4b80291830ebf62b41c6c7
Victor <victorgaspalmeida@hotmail.com>
2023-07-04 20:11:13 -0300

    feat: create UserRepository

---
+++ src/main/java/com/Reviewz/infrastructure/config/db/UserRepository.java
@@ -0,0 +1,7 @@
+package com.Reviewz.infrastructure.config.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserSchema, Long>{
+
+}
