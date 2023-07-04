af459ed0f51f06d161f04437191ad73b4d891606
Victor <victorgaspalmeida@hotmail.com>
2023-07-04 16:33:17 -0300

    feat: user entity

--- src/main/java/com/Reviewz/ReviewzApplication.java
+++ src/main/java/com/Reviewz/ReviewzApplication.java
@@ -1,13 +1,13 @@
-package com.Reviewz.demo;
+package com.Reviewz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ReviewzApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ReviewzApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/Reviewz/entity/user/User.java
@@ -0,0 +1,55 @@
+package com.Reviewz.entity.user;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	
+	public User(Long id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public User(String name, String email, String password) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+	
+}
