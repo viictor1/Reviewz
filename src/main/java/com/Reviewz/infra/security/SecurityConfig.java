5dba865044b443473df8d21d8664831ec0f88df9
Victor <victorgaspalmeida@hotmail.com>
2023-07-08 19:18:41 -0300

    feat: authorizing requests on securityConfig

--- src/main/java/com/Reviewz/infra/security/SecurityConfig.java
+++ src/main/java/com/Reviewz/infra/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Reviewz.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfig {
 		return httpSecurity
 				.csrf(crsf -> crsf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(req ->{
+                	req.requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN"); 
+                	req.anyRequest().authenticated();
+;                })
 				.build();
 				
 	}
