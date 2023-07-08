e260105a94e794d0dd35c02970d3009c334c3b98
Victor <victorgaspalmeida@hotmail.com>
2023-07-08 19:30:17 -0300

    feat: adding authenticationManager and PasswordEncoder bean to SecurityConfig

--- src/main/java/com/Reviewz/infra/security/SecurityConfig.java
+++ src/main/java/com/Reviewz/infra/security/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.Reviewz.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,8 +24,18 @@ public class SecurityConfig {
 				.authorizeHttpRequests(req ->{
                 	req.requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN"); 
                 	req.anyRequest().authenticated();
-;                })
+                })
 				.build();
 				
 	}
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
