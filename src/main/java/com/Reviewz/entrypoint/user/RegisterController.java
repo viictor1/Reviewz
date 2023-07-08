40a1061d5739b394a319ee792564b3c4885d3e9a
Victor <victorgaspalmeida@hotmail.com>
2023-07-08 19:34:35 -0300

    refactor: CreateUserController -> RegisterController

--- src/main/java/com/Reviewz/entrypoint/user/RegisterController.java
+++ src/main/java/com/Reviewz/entrypoint/user/RegisterController.java
@@ -3,6 +3,7 @@ package com.Reviewz.entrypoint.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,16 @@ import com.Reviewz.core.user.usecase.CreateUserUseCase;
 import com.Reviewz.core.user.usecase.CreateUserUseCase.Input;
 
 @RestController
-public class CreateUserController {
+@RequestMapping("/auth")
+public class RegisterController {
 
 	private CreateUserUseCase createUserUseCase;
 	
-	public CreateUserController(CreateUserUseCase createUserUseCase) {
+	public RegisterController(CreateUserUseCase createUserUseCase) {
 		this.createUserUseCase = createUserUseCase;
 	}
 	
-	@PostMapping("/user")
+	@PostMapping("/register")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createUser(@RequestBody Request request) throws Exception {
 		createUserUseCase.execute(new Input(request.name, request.email, request.password));
