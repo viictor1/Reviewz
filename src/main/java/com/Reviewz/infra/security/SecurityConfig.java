ce22270190feef862e570cb82c262637ea99f8bd
Victor <victorgaspalmeida@hotmail.com>
2023-07-08 16:39:53 -0300

    feat: SecurityConfig

---
+++ src/main/java/com/Reviewz/infra/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.Reviewz.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.csrf(crsf -> crsf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+				
+	}
+}
