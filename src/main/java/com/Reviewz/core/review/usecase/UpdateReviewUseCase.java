78d1b3ad716c5e9f7626c1eac8a4fabe3dfadcb1
Victor <victorgaspalmeida@hotmail.com>
2023-08-07 10:58:10 -0300

    feat/refactor: fixing UpdateReviewUseCase and adding verifications

--- src/main/java/com/Reviewz/core/review/usecase/UpdateReviewUseCase.java
+++ src/main/java/com/Reviewz/core/review/usecase/UpdateReviewUseCase.java
@@ -4,14 +4,12 @@ import com.Reviewz.core.genericException.ValidationError;
 import com.Reviewz.core.review.contract.ReviewGateway;
 import com.Reviewz.core.review.model.Review;
 import com.Reviewz.infra.dataprovider.schema.review.ReviewSchema;
-import com.Reviewz.infra.dataprovider.schema.user.UserSchema;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.AccessDeniedException;
 import java.util.Date;
 
 import static com.Reviewz.core.user.utils.UserUtils.getIdByToken;
-import static com.Reviewz.core.user.utils.UserUtils.retrieveUserFromToken;
 
 @Service
 public class UpdateReviewUseCase {
@@ -30,20 +28,30 @@ public class UpdateReviewUseCase {
             throw new AccessDeniedException("Cannot access another users review");
         }
 
-        Review updatedReview = setReviewFromInput(input);
+        var updatedReview = updateReviewFromInput(oldReview, input);
         reviewGateway.save(new ReviewSchema(updatedReview));
     }
 
-    public Review setReviewFromInput(Input input) throws ValidationError {
-        Review review = new Review();
-        review.setTitle(input.title);
-        review.setMadeBy(input.madeBy);
-        review.setCategory(input.category);
-        review.setStars(input.stars);
-        review.setReview(input.review);
-        review.setPublishedAt(input.publishedAt);
-        review.setReviewedAt(new Date());
-        review.setUser(retrieveUserFromToken(input.userToken));
+    public Review updateReviewFromInput(Review review, Input input) throws ValidationError {
+        if (input.title != null) {
+            review.setTitle(input.title);
+        }
+        if (input.madeBy != null) {
+            review.setMadeBy(input.madeBy);
+        }
+        if (input.category != null) {
+            review.setCategory(input.category);
+        }
+        if (input.stars != review.getStars()) {
+            review.setStars(input.stars);
+        }
+        if (input.review != null) {
+            review.setReview(input.review);
+        }
+        if (input.publishedAt != null) {
+            review.setPublishedAt(input.publishedAt);
+        }
+
         return review;
     }
 
